2f8ba2573fafb1604b3436b5bcb05018b00f4b7e
Nuno Torres <nagltorres@gmail.com>
2018-09-15 00:10:41 +0100

    Elm 0.18 Castle of Elm Working version

--- src/GameModel.elm
+++ src/GameModel.elm
@@ -1,8 +1,9 @@
-module GameModel where
+module GameModel exposing (..)
 
-import List exposing (map, concat, indexedMap, head, drop)
-import Graphics.Element exposing (..)
-import Graphics.Collage exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import List exposing (concat, drop, head, indexedMap, map)
+
 
 type Direction
     = Left
@@ -10,6 +11,7 @@ type Direction
     | Up
     | Down
     | None
+
 
 type Action
     = NoOp
@@ -19,8 +21,9 @@ type Action
 type alias Character =
     { x : Float
     , y : Float
-    , dir: Direction
+    , dir : Direction
     }
+
 
 type BackGroundTile
     = Floor --random floor tile
@@ -28,8 +31,10 @@ type BackGroundTile
     | Wall
     | Water
 
+
 type ShadowTile
     = Main
+
 
 type Tile
     = Door Size
@@ -44,205 +49,476 @@ type Tile
     | BackGround BackGroundTile
     | Shadow ShadowTile
 
+
 type Size
     = Small
     | Big
+
 
 type Condition
     = Ruined
     | Fine
 
+
 type WallJunction
     = Flat
     | Empty
 
-type alias WallTile = { r: WallJunction, l: WallJunction, u: WallJunction, d: WallJunction }
+
+type alias WallTile =
+    { r : WallJunction, l : WallJunction, u : WallJunction, d : WallJunction }
+
 
 type Progress
     = InProgress
     | GameOver
     | Won
 
-type alias Grid = List Tile --more than 1 layer
+
+type alias Grid =
+    List Tile
+
+
+
+--more than 1 layer
+
 
 type alias Model =
-    { grid: Grid
-    , gridSide: Float
+    { grid : Grid
+    , gridSide : Float
     , pc : Character
-}
+    }
+
 
 ffff : WallTile
-ffff = { r = Flat, u = Flat, l = Flat, d = Flat }
+ffff =
+    { r = Flat, u = Flat, l = Flat, d = Flat }
+
+
 efef : WallTile
-efef = { r = Empty, u = Flat, l = Empty, d = Flat }
+efef =
+    { r = Empty, u = Flat, l = Empty, d = Flat }
+
+
 feef : WallTile
-feef = { r = Flat, u = Empty, l = Empty, d = Flat }
+feef =
+    { r = Flat, u = Empty, l = Empty, d = Flat }
+
+
 eeff : WallTile
-eeff = { r = Empty, u = Empty, l = Flat, d = Flat }
+eeff =
+    { r = Empty, u = Empty, l = Flat, d = Flat }
+
+
 fefe : WallTile
-fefe = { r = Flat, u = Empty, l = Flat, d = Empty }
+fefe =
+    { r = Flat, u = Empty, l = Flat, d = Empty }
+
+
 ffee : WallTile
-ffee = { r = Flat, u = Flat, l = Empty, d = Empty }
+ffee =
+    { r = Flat, u = Flat, l = Empty, d = Empty }
+
+
 effe : WallTile
-effe = { r = Empty, u = Flat, l = Flat, d = Empty }
+effe =
+    { r = Empty, u = Flat, l = Flat, d = Empty }
+
+
 feee : WallTile
-feee = { r = Flat, u = Empty, l = Empty, d = Empty }
+feee =
+    { r = Flat, u = Empty, l = Empty, d = Empty }
+
+
 eefe : WallTile
-eefe = { r = Empty, u = Empty, l = Flat, d = Empty }
+eefe =
+    { r = Empty, u = Empty, l = Flat, d = Empty }
+
+
 efff : WallTile
-efff = { r = Empty, u = Flat, l = Flat, d = Flat }
+efff =
+    { r = Empty, u = Flat, l = Flat, d = Flat }
+
+
 ffef : WallTile
-ffef = { r = Flat, u = Flat, l = Empty, d = Flat }
+ffef =
+    { r = Flat, u = Flat, l = Empty, d = Flat }
+
+
 efee : WallTile
-efee = { r = Empty, u = Flat, l = Empty, d = Empty }
+efee =
+    { r = Empty, u = Flat, l = Empty, d = Empty }
+
+
 fffe : WallTile
-fffe = { r = Flat, u = Flat, l = Flat, d = Empty }
+fffe =
+    { r = Flat, u = Flat, l = Flat, d = Empty }
+
 
 mainGrid : Grid
-mainGrid = [BackGround (WallOver ffee), BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef), --first line
-            BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver effe),
+mainGrid =
+    [ BackGround (WallOver ffee)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , --first line
+      BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver effe)
+    , BackGround (WallOver fefe)
+    , BackGround Wall
+    , BackGround Wall
+    , -- second line
+      BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fffe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver feee)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efee)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver eeff)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver ffef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver eefe)
+    , BackGround (WallOver fefe)
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround (WallOver fefe)
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver feef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efff)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver fefe)
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround Floor
+    , BackGround (WallOver fefe)
+    , BackGround (WallOver feef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver efef)
+    , BackGround (WallOver eeff)
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , --last line
+      BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Wall
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , --last line
+      BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    , BackGround Water
+    ]
 
-            BackGround (WallOver fefe), BackGround Wall, BackGround Wall, -- second line
-            BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall,
-            BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround (WallOver fefe),
 
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
+gridSize =
+    15
 
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround (WallOver fffe), BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
 
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
+tileSize =
+    64
 
-            BackGround (WallOver feee), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efee), BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver eeff), BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround (WallOver ffef), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver eefe),
 
-            BackGround (WallOver fefe), BackGround Wall, BackGround Wall, BackGround (WallOver fefe), BackGround Wall,
-            BackGround Wall,
-            BackGround Wall, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Wall, BackGround Wall,
-            BackGround Wall, BackGround Wall, BackGround (WallOver fefe),
-
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor,
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround (WallOver feef), BackGround (WallOver efef),
-            BackGround (WallOver efff),
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor,
-            BackGround Wall, BackGround Wall, BackGround Wall, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-
-            BackGround (WallOver fefe), BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor, BackGround Floor,
-            BackGround Floor, BackGround Floor, BackGround (WallOver fefe),
-
-            BackGround (WallOver feef), BackGround (WallOver efef), BackGround (WallOver efef),  BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver efef),
-            BackGround (WallOver efef), BackGround (WallOver efef), BackGround (WallOver eeff),
-
-            BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, --last line
-            BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall, BackGround Wall,
-            BackGround Wall, BackGround Wall, BackGround Wall,
-
-            BackGround Water, BackGround Water, BackGround Water, BackGround Water, BackGround Water, BackGround Water, --last line
-            BackGround Water, BackGround Water, BackGround Water, BackGround Water, BackGround Water, BackGround Water,
-            BackGround Water, BackGround Water, BackGround Water]
-
-gridSize = 15
-tileSize = 64
-
-getListIdx: Int -> Grid -> Maybe Tile
+getListIdx : Int -> Grid -> Maybe Tile
 getListIdx idx list =
     head (drop idx list)
+
+
 
 {----------------------------------------------------
                     Tile Functions
 -----------------------------------------------------}
 
+
 checkWallImg walltype =
-  let
-    getsrc side =
-      case side of
-        Flat ->
-          "flat"
-        Empty ->
-          "empty"
-    r = getsrc walltype.r
-    l = getsrc walltype.l
-    u = getsrc walltype.u
-    d = getsrc walltype.d
-  in
+    let
+        getsrc side =
+            case side of
+                Flat ->
+                    "flat"
+
+                Empty ->
+                    "empty"
+
+        r =
+            getsrc walltype.r
+
+        l =
+            getsrc walltype.l
+
+        u =
+            getsrc walltype.u
+
+        d =
+            getsrc walltype.d
+    in
     "img/walls/" ++ r ++ "-" ++ u ++ "-" ++ l ++ "-" ++ d ++ ".png"
 
 
 checkBgImg bgtype =
-  case bgtype of
-    Floor -> "img/floor/floor_01.png"
-    Wall  -> "img/walls/wall.png"
-    Water -> "img/water/water_01.png"
-    WallOver tile -> checkWallImg tile
+    case bgtype of
+        Floor ->
+            "img/floor/floor_01.png"
+
+        Wall ->
+            "img/walls/wall.png"
+
+        Water ->
+            "img/water/water_01.png"
+
+        WallOver tile ->
+            checkWallImg tile
+
 
 displayTile : Tile -> Element
 displayTile tile =
     let
-      src =
-        case tile of
-          BackGround tiletype ->
-            checkBgImg tiletype
-          _ ->""
-    in
-      image tileSize tileSize src
+        src =
+            case tile of
+                BackGround tiletype ->
+                    checkBgImg tiletype
 
-displayTileAtCoordinates : (Tile, Int, Int) -> Form
-displayTileAtCoordinates (t,i,j) =
-    let position =
-          ( (toFloat tileSize) * (toFloat i - (toFloat gridSize - 1)/2)
-        , (-1) * (toFloat tileSize) * (toFloat j - (toFloat gridSize - 1)/2))
+                _ ->
+                    ""
     in
-        move position <| toForm <| displayTile t
+    image tileSize tileSize src
+
+
+displayTileAtCoordinates : ( Tile, Int, Int ) -> Form
+displayTileAtCoordinates ( t, i, j ) =
+    let
+        position =
+            ( toFloat tileSize * (toFloat i - (toFloat gridSize - 1) / 2)
+            , -1 * toFloat tileSize * (toFloat j - (toFloat gridSize - 1) / 2)
+            )
+    in
+    move position <| toForm <| displayTile t
+
 
 displayTileAtIndex : Int -> Tile -> Form
 displayTileAtIndex index tile =
     let
-        y =  index // gridSize
-        x =  index % gridSize
+        y =
+            index // gridSize
+
+        x =
+            index % gridSize
     in
-        displayTileAtCoordinates (tile, x, y)
+    displayTileAtCoordinates ( tile, x, y )
 
-gridWidth : Float -- the width of the entire game grid
-gridWidth = (toFloat gridSize) * (toFloat tileSize)
 
-getTileIdxFromPosition : (Float, Float) -> Int
-getTileIdxFromPosition (x, y) =
+
+{--| the width of the entire game grid
+--}
+
+
+gridWidth : Float
+gridWidth =
+    toFloat gridSize * toFloat tileSize
+
+
+getTileIdxFromPosition : ( Float, Float ) -> Int
+getTileIdxFromPosition ( x, y ) =
     let
-      x_tile = (round x) + 7
-      y_tile = 8 - (round y)
-    in
-      (y_tile - 1) * gridSize + x_tile
+        x_tile =
+            round x + 7
 
-displayGrid : (Int, Int) -> (Float, Float) -> Grid -> List Form -- display a grid
+        y_tile =
+            8 - round y
+    in
+    (y_tile - 1) * gridSize + x_tile
+
+
+displayGrid :
+    ( Int, Int )
+    -> ( Float, Float )
+    -> Grid
+    -> List Form -- display a grid
 displayGrid frame pcCoords g =
     let
-      tiles = indexedMap displayTileAtIndex g
+        tiles =
+            indexedMap displayTileAtIndex g
     in
-      tiles
+    tiles
+
+
 
 -- collisions : all except floor
--- src/MainLoop.elm
+++ src/MainLoop.elm
@@ -1,50 +1,113 @@
-import Window
-import Debug exposing (watch, log)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (image, Element)
-import Keyboard
-import Signal exposing (Signal, map, merge, map2, foldp)
+module Main exposing (..)
+
+--import Signal exposing (Signal, foldp, map, map2, merge)
+--import Graphics.Element exposing (Element, image)
+
+import Collage exposing (..)
+import Debug exposing (log)
+import Element exposing (Element, image)
 import GameModel exposing (..)
+import Html exposing (Html)
+import Keyboard
+
+
+--import Window
+
 
 pcState : Character
-pcState = { x = 0, y = 0, dir = Right } -- tiredness strenght blabla
+pcState =
+    { x = 0, y = 0, dir = Right }
 
-model : Model
-model =
+
+
+-- tiredness strenght blabla
+
+
+initialModel : Model
+initialModel =
     { grid = mainGrid
     , gridSide = gridWidth
-    , pc = pcState }
+    , pc = pcState
+    }
+
+
+
+-- Msg
+
+
+type Msg
+    = Noop
+    | KeyPress Direction
+
+
 
 -- UPDATE
 
-update : Direction -> Model -> Model
-update dir model =
-    model
-        |> movepc dir
-        -- if into monster slash
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Noop ->
+            ( model, Cmd.none )
+
+        KeyPress dir ->
+            ( model
+                |> movepc dir
+            , Cmd.none
+            )
+
+
+
+-- if into monster slash
+
 
 movepc : Direction -> Model -> Model
 movepc dir model =
-  let
-    checkPc default pc =
-      let
-        x = pc.x |> Debug.watch "pc x"
-        y = pc.y |> Debug.watch "pc y"
-        idx = getTileIdxFromPosition (pc.x, pc.y) |> Debug.watch "idx"
-        tile = getListIdx idx model.grid |> Debug.watch "tile"
-      in
-        case tile of
-          Nothing -> pc
-          Just tilet -> if tilet == BackGround Floor then pc else default
-    updatePc pc dir =
-      case dir of
-        Up ->  { pc |  y = pc.y + 1, dir = Up }
-        Down -> { pc | y = pc.y - 1, dir = Down }
-        Left -> { pc | x = pc.x - 1, dir = Left }
-        Right -> { pc | x = pc.x + 1, dir = Right }
-        None -> pc
-  in
-    { model | pc = (checkPc model.pc (updatePc model.pc dir)) }
+    let
+        checkPc default pc =
+            let
+                x =
+                    pc.x |> Debug.log "pc x"
+
+                y =
+                    pc.y |> Debug.log "pc y"
+
+                idx =
+                    getTileIdxFromPosition ( pc.x, pc.y ) |> Debug.log "idx"
+
+                tile =
+                    getListIdx idx model.grid |> Debug.log "tile"
+            in
+            case tile of
+                Nothing ->
+                    pc
+
+                Just tilet ->
+                    if tilet == BackGround Floor then
+                        pc
+                    else
+                        default
+
+        updatePc pc dir =
+            case dir of
+                Up ->
+                    { pc | y = pc.y + 1, dir = Up }
+
+                Down ->
+                    { pc | y = pc.y - 1, dir = Down }
+
+                Left ->
+                    { pc | x = pc.x - 1, dir = Left }
+
+                Right ->
+                    { pc | x = pc.x + 1, dir = Right }
+
+                None ->
+                    pc
+    in
+    { model | pc = checkPc model.pc (updatePc model.pc dir) }
+
+
 
 -- on which tile it ends up
 -- which other tiles become visible
@@ -52,61 +115,190 @@ movepc dir model =
 -- or be attacked
 -- update monsters paths and spawn them
 -- update time ticking
-
 -- what is the center (PC)
-
 -- VIEW
-matchToSide : (Int, Int) -> Int -> (Int, Int)
-matchToSide frame side =
-  let
-    ( w, h ) = frame
-    tW =  w // side
-    tH =  h // side
-  in
-    ((log "tW" tW) , (log "tH" tH) )
 
-view : (Int, Int) -> Model -> Element
-view frame model =
+
+matchToSide : ( Int, Int ) -> Int -> ( Int, Int )
+matchToSide frame side =
     let
-      tileSide = 64
-      dir =
-        case model.pc.dir of
-          Left -> "left"
-          Right -> "right"
-          Up -> "up"
-          Down -> "down"
-          _ -> "none"
-      src = "img/pc/" ++ dir ++".png" -- Hardcoded
-      pcImage = image tileSide tileSide src
-      (tW, tH) = matchToSide (log "win" frame) tileSide
-      tWSide = (tW * tileSide)
-      tHSide = (tH * tileSide)
-      pcPos = (model.pc.x * tileSide, tileSide * model.pc.y)
+        ( w, h ) =
+            frame
+
+        tW =
+            w // side
+
+        tH =
+            h // side
     in
-      collage tWSide tHSide ((displayGrid (tW, tH) pcPos mainGrid) ++ [pcImage
-                                                                             |> toForm
-                                                                             |> Debug.trace "pc"
-                                                                             |> move pcPos])
+    ( log "tW" tW, log "tH" tH )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        tileSide =
+            64
+
+        frame =
+            ( 600, 600 )
+
+        dir =
+            case model.pc.dir of
+                Left ->
+                    "left"
+
+                Right ->
+                    "right"
+
+                Up ->
+                    "up"
+
+                Down ->
+                    "down"
+
+                _ ->
+                    "none"
+
+        src =
+            "img/pc/" ++ dir ++ ".png"
+
+        -- Hardcoded
+        pcImage =
+            image tileSide tileSide src
+
+        ( tW, tH ) =
+            matchToSide (log "win" frame) tileSide
+
+        tWSide =
+            tW * tileSide
+
+        tHSide =
+            tH * tileSide
+
+        pcPos =
+            ( model.pc.x * tileSide, tileSide * model.pc.y )
+
+        theelement =
+            collage tWSide
+                tHSide
+                (displayGrid ( tW, tH ) pcPos mainGrid
+                    ++ [ pcImage
+                            |> toForm
+                            |> Debug.log "pc"
+                            |> move pcPos
+                       ]
+                )
+    in
+    theelement
+        |> Element.toHtml
+
+
+
+-- MAIN
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    --Keyboard.presses (\code ->  (Char.fromCode code))
+    Sub.batch
+        [ Keyboard.downs (\kcode -> KeyPress (fromCode kcode))
+
+        --, Keyboard.ups (\kcode -> KeyUpMsg (fromCode kcode))
+        --Keyboard.presses (\kcode -> KeyPress (fromCode kcode))
+        --, Time.every (msPerFrame model * Time.millisecond) StepNoKey
+        ]
+
+
+fromCode : Int -> Direction
+fromCode keyCode =
+    case keyCode of
+        79 ->
+            Left
+
+        37 ->
+            Left
+
+        80 ->
+            Right
+
+        39 ->
+            Right
+
+        81 ->
+            Up
+
+        38 ->
+            Up
+
+        40 ->
+            Down
+
+        65 ->
+            Down
+
+        _ ->
+            None
+
+
 
 -- SIGNALS
+--main : Signal Element
+--main = lift2 display Window.dimensions (foldp step mario input)
 
-main : Signal Element
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+main : Program Never Model Msg
 main =
-  map2 view Window.dimensions (foldp update model input)
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
+
+
+-- SIGNALS
+--main : Signal Element
+--main =
+--    map2 view Window.dimensions (foldp update model input)
 -- INPUTS: CONTROLS
-inputDir : Signal Direction
-inputDir =
-  let dir { x, y }  =
-      case (x, y) of
-        (  0,  1 ) -> Up
-        (  0, -1 ) -> Down
-        (  1,  0 ) -> Right
-        ( -1,  0 ) -> Left
-        _ -> None
-  in merge (Signal.map dir Keyboard.arrows) (Signal.map dir Keyboard.wasd)
+{-
+   inputDir : Signal Direction
+   inputDir =
+       let
+           dir { x, y } =
+               case ( x, y ) of
+                   ( 0, 1 ) ->
+                       Up
 
--- samples arrows when fps tick
-input : Signal Direction
-input =
-    map (Debug.watch "direction") inputDir
+                   ( 0, -1 ) ->
+                       Down
+
+                   ( 1, 0 ) ->
+                       Right
+
+                   ( -1, 0 ) ->
+                       Left
+
+                   _ ->
+                       None
+       in
+       merge (Signal.map dir Keyboard.arrows) (Signal.map dir Keyboard.wasd)
+
+
+
+   -- samples arrows when fps tick
+
+
+   input : Signal Direction
+   input =
+       map (Debug.watch "direction") inputDir
+
+
+-}
